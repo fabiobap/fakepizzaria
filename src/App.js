15288b0907490db54e7bcb93fe0af91f93e7fab1
Fabio <fabio.bap88@gmail.com>
2020-03-09 00:45:43 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components'
+import { Navbar } from './Navbar/Navbar';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    margin:0;
     font-family: 'Open Sans', sans-serif;
   }
     h1,h2,h3 {
@@ -12,9 +14,9 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-    <h1>FakePizzaria</h1>
-    <GlobalStyle/>
-    <div>Hello world :)</div>
+      <GlobalStyle />
+      <Navbar />
+      <div>Hello world :)</div>
     </>
   );
 }
