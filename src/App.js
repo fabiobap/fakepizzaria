526ef7d8e346d648a7e26d9d66131c18e05d4a57
Fabio <fabio.bap88@gmail.com>
2020-03-09 00:20:42 -0300

    add google fonts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
   }
+    h1,h2,h3 {
+    font-family: 'Righteous', cursive;
+    }
 `
 function App() {
   return (
     <>
+    <h1>FakePizzaria</h1>
     <GlobalStyle/>
     <div>Hello world :)</div>
     </>
