cd565c02c47b5e51c8f5185655e295e8255481a0
Fabio <fabio.bap88@gmail.com>
2020-03-10 00:09:17 -0300

    Added Order and more FoodDialog

---
+++ src/Order/Order.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ConfirmButton, DialogFooter, DialogContent } from '../FoodDialog/FoodDialog';
+
+const OrderStyled = styled.div`
+position: fixed;
+right: 0px;
+top: 48px;
+width: 340px;
+height: calc(100% - 48px);
+background-color: white;
+z-index: 10;
+box-shadow: 4px 0px 5px 4px grey;
+display:flex;
+flex-direction: column;
+`;
+
+const OrderContent = styled(DialogContent)`
+padding: 20px;
+height: 100%;
+`;
+
+export function Order() {
+    return (
+        <OrderStyled>
+            <OrderContent>Your order is empty!</OrderContent>
+            <DialogFooter>
+                <ConfirmButton>Checkout</ConfirmButton>
+            </DialogFooter>
+        </OrderStyled>
+    );
+}
