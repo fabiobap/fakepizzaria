509706b89ade1d68c899d00c1f08235c96b50bca
Fabio <fabio.bap88@gmail.com>
2020-03-13 23:39:33 -0300

    Firebase database config, add order to database

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -9,6 +9,8 @@ const NavbarStyled = styled.div`
     position:fixed;
     width:100%;
     z-index: 999;
+    display: flex;
+    justify-content: space-between
 `;
 
 const Logo = styled(Title)`
@@ -16,11 +18,34 @@ font-size: 20px;
 color: white;
 text-shadow: 5px 3px 2px #380502;
 `;
+const UserStatus = styled.div`
+color: white;
+font-size: 12px;
+margin-right: 30px;
+`;
 
-export function Navbar() {
+const LoginButton = styled.span`
+cursor: pointer;
+`;
+
+export function Navbar({ login, loggedIn, logout }) {
     return <NavbarStyled>
         <Logo>
             Fake Pizzaria <span role='img' aria-label="slicepizza">🍕</span>
         </Logo>
+        <UserStatus>
+            {loggedIn !== 'loading' ? (
+                <>
+                    👤{loggedIn ? `Hello ${loggedIn.displayName}` : ""}
+                    {loggedIn ? (
+                        <LoginButton onClick={logout}>Log out</LoginButton>
+                    ) : (
+                            <LoginButton onClick={login}>Log in / Sign up</LoginButton>
+                        )}
+                </>
+            ) : (
+                    "loading..."
+                )}
+        </UserStatus>
     </NavbarStyled>
 }
--- src/Order/Order.js
+++ src/Order/Order.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { ConfirmButton, DialogFooter, DialogContent, getPrice } from '../FoodDialog/FoodDialog';
 import { formatPrice } from '../Data/FoodData';
 
+const database = window.firebase.database();
+
 const OrderStyled = styled.div`
 position: fixed;
 right: 0px;
@@ -47,7 +49,34 @@ color: gray;
 font-size: 10px;
 `;
 
-export function Order({ orders, setOrders, setOpenFood }) {
+function sendOrder(orders, { email, displayName }) {
+    const newOrderRef = database.ref('orders').push();
+    const newOrders = orders.map(order => {
+        return Object.keys(order).reduce((acc, orderKey) => {
+            if (!order[orderKey]) {
+                return acc;
+            }
+            if (orderKey === "toppings") {
+                return {
+                    ...acc,
+                    [orderKey]: order[orderKey]
+                        .filter(({ checked }) => checked)
+                        .map(({ name }) => name)
+                };
+            }
+            return {
+                ...acc,
+                [orderKey]: order[orderKey]
+            };
+        }, {});
+    });
+    newOrderRef.set({
+        order: newOrders,
+        email, displayName
+    });
+}
+
+export function Order({ orders, setOrders, setOpenFood, login, loggedIn }) {
     const subtotal = orders.reduce((total, order) => {
         return total + getPrice(order);
     }, 0);
@@ -119,7 +148,14 @@ export function Order({ orders, setOrders, setOpenFood }) {
                     </OrderContent>
                 )}
             <DialogFooter>
-                <ConfirmButton>Checkout</ConfirmButton>
+                <ConfirmButton onClick={() => {
+                    if (loggedIn) {
+                        sendOrder(orders, loggedIn)
+                    } else {
+                        login();
+                    }
+                }
+                }>Checkout</ConfirmButton>
             </DialogFooter>
         </OrderStyled>
     );
